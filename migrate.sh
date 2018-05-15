082a9395950d93a4c94e7e36503cace3cad81081
Shunji Makino <shunji.makino@gmail.com>
2018-05-15 11:52:36 +0900

    fix migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -1,3 +1,4 @@
-ln -s ~/ghq.github.com/macinjoke/my-vimrc ~/.vimrc
-ln -s ~/ghq.github.com/macinjoke/ideavimrc ~/.ideavimrc
-ln -s ~/ghq.github.com/macinjoke/ftplugin ~/.vim/ftplugin
+ln -s ~/.ghq/github.com/macinjoke/my-vimrc/vimrc ~/.vimrc
+ln -s ~/.ghq/github.com/macinjoke/my-vimrc/ideavimrc ~/.ideavimrc
+mkdir ~/.vim
+ln -s ~/.ghq/github.com/macinjoke/my-vimrc/ftplugin ~/.vim/ftplugin
