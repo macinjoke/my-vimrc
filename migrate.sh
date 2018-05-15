9af63bef3f47ce84cdfd51d04a9d8f9d35ee5c62
Shunji Makino <shunji.makino@gmail.com>
2018-05-15 11:57:54 +0900

    update migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -1,4 +1,11 @@
 ln -s ~/.ghq/github.com/macinjoke/my-vimrc/vimrc ~/.vimrc
 ln -s ~/.ghq/github.com/macinjoke/my-vimrc/ideavimrc ~/.ideavimrc
 mkdir ~/.vim
+mkdir ~/.vim/swp
 ln -s ~/.ghq/github.com/macinjoke/my-vimrc/ftplugin ~/.vim/ftplugin
+
+# neobundleのインストール
+# もしかしたらinstall 方法が変わるかも 2018/5/15
+mkdir ~/.vim/bundle
+git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+
